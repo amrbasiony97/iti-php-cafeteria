bc922be5fde3601e320b62be926d27baf8323e6d
Ayman Salah <ayman.1551.salah@gmail.com>
2023-04-28 21:35:56 +0300

    Index

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -3,9 +3,27 @@
 class Product
 {
     private static $table = 'products';
-    
+
     static function getAll()
     {
         return Database::select(self::$table);
+    }
+
+    static function getOneProduct($id)
+    {
+    }
+
+    static function createOneProduct()
+    {
+        
+    }
+
+
+    static function updateOneProduct($id, $data)
+    {
+    }
+
+    static function deleteProduct($id)
+    {
     }
 }
--- app/Views/Product/index.php
+++ app/Views/Product/index.php
@@ -4,9 +4,37 @@
 
 <ul>
     <?php
+    echo "<div class='container'>" ; 
+    echo "<div class='row'>" ; 
     foreach ($data['allData'] as $product) {
-        echo "<li>{$product['name']}</li>";
+        echo "<div class='col-md-4 col-sm-4  profile_details'>
+                        <div class='well profile_view'>
+                          <div class='col-sm-12'>
+                            <h4 class='brief'><i>RRRRRR</i></h4>
+                            <div class='left col-md-7 col-sm-7'>
+                              <p><strong>About: </strong> Web Designer / UX / Graphic Artist / Coffee Lover </p>
+                            </div>
+                            <div class='right col-md-5 col-sm-5 text-center'>
+                              <img src='images/img.jpg' alt='' class='img-circle img-fluid'>
+                            </div>
+                          </div>
+                          <div class=' profile-bottom text-center'>
+                            <div class=' col-sm-6 emphasis'>
+                             
+                            </div>
+                            <div class=' col-sm-6 emphasis'>
+                              <button type='button' class='btn btn-success btn-sm'> <i class='fa fa-user'>
+                                </i> <i class='fa fa-comments-o'></i> </button>
+                              <button type='button' class='btn btn-primary btn-sm'>
+                                <i class='fa fa-user'> </i> View Product
+                              </button>
+                            </div>
+                          </div>
+                        </div>
+                      </div>";
     }
+     echo "</div>" ; 
+     echo "</div>" ; 
     ?>
 </ul>
 <?php include(VIEWS . 'include/footer.php') ?>
