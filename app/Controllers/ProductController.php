062e345e1fa844f339460f4fe884f5633993de05
Ayman Salah <ayman.1551.salah@gmail.com>
2023-04-28 20:56:19 +0300

    Product Index

--- app/Controllers/ProductController.php
+++ app/Controllers/ProductController.php
@@ -1,17 +1,19 @@
 <?php
 // For Controlling /product
 
+// View::load('Home/index');
 
-// View::load('Home/index'); 
 
 class ProductController
 {
     // For Printing all 
     public function index()
     {
-        $allProducts = Product::getAll()  ;
-        var_dump($allProducts) ;
-        echo "This is Index For Product" ;
-        View::load('Product/index');  
+        $allProducts = Product::getAll();
+        // var_dump($allProducts);
+        // echo "This is Index For Product" ;
+        View::load('Product/index',$data =  [
+            'allData' => $allProducts
+        ]);
     }
 }
--- app/Views/Product/index.php
+++ app/Views/Product/index.php
@@ -1,2 +1,12 @@
-<?php 
-echo "This is Index Product View" ; 
+<?php include(VIEWS . 'include/header.php') ?>
+<a href="<?php route('product/index') ?>">All Products</a>
+<h1>Products Index</h1>
+
+<ul>
+    <?php
+    foreach ($data['allData'] as $product) {
+        echo "<li>{$product['name']}</li>";
+    }
+    ?>
+</ul>
+<?php include(VIEWS . 'include/footer.php') ?>
