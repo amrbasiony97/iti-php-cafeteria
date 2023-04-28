9468d330ab82bbab195b95c4a7f034b8c9c4879b
Ayman Salah <ayman.1551.salah@gmail.com>
2023-04-28 19:59:33 +0300

    Product

--- app/Controllers/ProductController.php
+++ app/Controllers/ProductController.php
@@ -1,5 +1,10 @@
 <?php
-
+// For Controlling /product
 class ProductController
 {
+    // For Printing all 
+    public function index()
+    {
+        echo "This is Index For Product" ; 
+    }
 }
