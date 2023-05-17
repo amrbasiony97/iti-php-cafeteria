e8ac6f02313e339ad0204fece51c05de5b59caec
swe-amr-abdelaziz <swe.amr.abdelaziz@gmail.com>
2023-05-18 00:28:26 +0300

    Added Login page without authentication

--- app/Controllers/AuthController.php
+++ app/Controllers/AuthController.php
@@ -2,28 +2,8 @@
 
 class AuthController
 {
-    public function register()
-    {
-        var_dump(User::getAll());
-    }
-
     public function login()
     {
-        $count = Database::insert('users', [
-            'name' => 'Amr Basiony',
-            'email' => 'amrbasiony97@gmail.com',
-            'password' => 'password123',
-            'room_no' => '101',
-            'ext' => 123,
-            'image' => 'image1.jpg'
-        ]);
-
-        if ($count) {
-            echo 'User created successfully';
-        }
-        else {
-            echo 'Error creating user';
-        }
+        View::load('Auth/login');
     }
-
 }
