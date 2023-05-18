608a6608a73050db94393624f4e6afac9861bdd0
swe-amr-abdelaziz <swe.amr.abdelaziz@gmail.com>
2023-05-18 22:07:15 +0300

    Updated layout

--- app/Config/config.php
+++ app/Config/config.php
@@ -14,7 +14,7 @@ define('PASSWORD', 'Iti#9634');
 
 define('BASE_URL', 'https://localhost/iti-php-cafeteria/public/');
 */
-define('USER', 'root');
-define('PASSWORD', '');
+define('USER', 'amr');
+define('PASSWORD', 'password');
 
 define('BASE_URL', 'http://localhost/iti-php-cafeteria/public/');
--- app/Views/Layouts/admin.php
+++ app/Views/Layouts/admin.php
@@ -74,13 +74,7 @@
                     <li><a href="#">Add Product</a></li>
                   </ul>
                 </li>
-                <li><a><i class="fa fa-list-alt"></i> Categories <span class="fa fa-chevron-down"></span></a>
-                  <ul class="nav child_menu">
-                    <li><a href="#">All Categories</a></li>
-                    <li><a href="#">Add Category</a></li>
-                  </ul>
-                </li>
-                <li><a><i class="fa fa-coffee"></i> Orders <span class="fa fa-chevron-down"></span></a>
+                <li><a><i class="fa fa-coffee"></i> Manual Order <span class="fa fa-chevron-down"></span></a>
                   <ul class="nav child_menu">
                     <li><a href="#">All Orders</a></li>
                     <li><a href="#">Add Order</a></li>
@@ -132,71 +126,6 @@
                   </li>
                   <li><a href="javascript:;">Help</a></li>
                   <li><a href="<?php route('auth/logout') ?>"><i class="fa fa-sign-out pull-right"></i> Log Out</a></li>
-                </ul>
-              </li>
-
-              <li role="presentation" class="dropdown">
-                <a href="javascript:;" class="dropdown-toggle info-number" data-toggle="dropdown" aria-expanded="false">
-                  <i class="fa fa-envelope-o"></i>
-                  <span class="badge bg-green">6</span>
-                </a>
-                <ul id="menu1" class="dropdown-menu list-unstyled msg_list" role="menu">
-                  <li>
-                    <a>
-                      <span class="image"><img src="<?php asset('gentelella/production/images/user.png') ?>" alt="Profile Image" /></span>
-                      <span>
-                        <span>John Smith</span>
-                        <span class="time">3 mins ago</span>
-                      </span>
-                      <span class="message">
-                        Film festivals used to be do-or-die moments for movie makers. They were where...
-                      </span>
-                    </a>
-                  </li>
-                  <li>
-                    <a>
-                      <span class="image"><img src="<?php asset('gentelella/production/images/user.png') ?>" alt="Profile Image" /></span>
-                      <span>
-                        <span>John Smith</span>
-                        <span class="time">3 mins ago</span>
-                      </span>
-                      <span class="message">
-                        Film festivals used to be do-or-die moments for movie makers. They were where...
-                      </span>
-                    </a>
-                  </li>
-                  <li>
-                    <a>
-                      <span class="image"><img src="<?php asset('gentelella/production/images/user.png') ?>" alt="Profile Image" /></span>
-                      <span>
-                        <span>John Smith</span>
-                        <span class="time">3 mins ago</span>
-                      </span>
-                      <span class="message">
-                        Film festivals used to be do-or-die moments for movie makers. They were where...
-                      </span>
-                    </a>
-                  </li>
-                  <li>
-                    <a>
-                      <span class="image"><img src="<?php asset('gentelella/production/images/user.png') ?>" alt="Profile Image" /></span>
-                      <span>
-                        <span>John Smith</span>
-                        <span class="time">3 mins ago</span>
-                      </span>
-                      <span class="message">
-                        Film festivals used to be do-or-die moments for movie makers. They were where...
-                      </span>
-                    </a>
-                  </li>
-                  <li>
-                    <div class="text-center">
-                      <a>
-                        <strong>See All Alerts</strong>
-                        <i class="fa fa-angle-right"></i>
-                      </a>
-                    </div>
-                  </li>
                 </ul>
               </li>
             </ul>
