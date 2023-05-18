73d67e443fc031a479d3a8957e70e29e3b7e7699
Ayman Salah <ayman.1551.salah@gmail.com>
2023-05-19 00:24:00 +0300

    Checking with POST

--- app/Controllers/CheckController.php
+++ app/Controllers/CheckController.php
@@ -15,20 +15,27 @@ class CheckController
 
     public function editCheckStatus()
     {
-        $id  = $_POST['id'];
-        $checkOrder =  Database::select_one('orders', $id);
-        if ($checkOrder['status'] == 'processing') {
-            $queryData = [];
-            $queryData['status'] = 'out for delivery';
-            $result = Database::update('orders', $_POST['id'], $queryData);
-            $allChecks  =  Database::select('orders');
-            View::load('Check/index', $data =  [
-                'allChecks' => $allChecks
-            ]);
-        } else if ($checkOrder['status'] == 'out for delivery') {
-            $queryData = [];
-            $queryData['status'] = 'done';
-            $result = Database::update('orders', $_POST['id'], $queryData);
+        if (isset($_POST['id'])) {
+            $id  = $_POST['id'];
+            $checkOrder =  Database::select_one('orders', $id);
+            if ($checkOrder['status'] == 'processing') {
+                $queryData = [];
+                $queryData['status'] = 'out for delivery';
+                $result = Database::update('orders', $_POST['id'], $queryData);
+                $allChecks  =  Database::select('orders');
+                View::load('Check/index', $data =  [
+                    'allChecks' => $allChecks
+                ]);
+            } else if ($checkOrder['status'] == 'out for delivery') {
+                $queryData = [];
+                $queryData['status'] = 'done';
+                $result = Database::update('orders', $_POST['id'], $queryData);
+                $allChecks  =  Database::select('orders');
+                View::load('Check/index', $data =  [
+                    'allChecks' => $allChecks
+                ]);
+            }
+        } else {
             $allChecks  =  Database::select('orders');
             View::load('Check/index', $data =  [
                 'allChecks' => $allChecks
